02b379eb522d662d924403fda169fb5527107f51
Mohammad Kamran <webhost01001@gmail.com>
2025-04-16 03:08:06 +0530

    ESLint instalation for --save-dev eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,24 @@
 module.exports = {
-    rules: {
-      "react/no-unescaped-entities": "off"
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/prop-types': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
     }
-  };
+  }
+};
   
